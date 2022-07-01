8fcf38c2e6b9f18b71c8af05620339c7aff9066d
frankguido <38305115+frankguido@users.noreply.github.com>
2022-07-01 07:45:31 -0500

    Add files via upload

---
+++ bdproyectolist.sql
@@ -0,0 +1,23 @@
+USE `db_proyecto` ;
+
+
+
+
+
+
+-- INSERT propietario
+insert into propietario values
+(null,"Bryan Alexander","Bernuy Bravo",71434952,"user1@gmail.com",980598055,"1997-05-03","2020-05-05",1);
+insert into propietario  values
+(null,"Marco Alberto","Vicenzi Gianetto",09621521,"user2@gmail.com",990456789,"1999-04-03","2020-05-05",1);
+insert into propietario  values
+(null,"Mariela Morena","Beltran Renitti",09621545,"user3@gmail.com",975456852,"1998-04-03","2020-07-05",1);
+insert into propietario  values
+(null,"Julio Miguel","Benedetti Saldaña",05555444,"user4@gmail.com",982632145,"1997-05-08","2022-05-05",1);
+insert into propietario  values
+(null,"Migue Angel","Quiroga Muñoz",41256635,"user5@gmail.com",963563255,"1982-04-02","2022-03-03",1);
+insert into propietario  values
+(null,"Manuel Enrique","Gonzales Muñoz",09940227,"user5@gmail.com",963563255,"1982-04-02","2022-03-03",0);
+
+SELECT * FROM propietario;
+

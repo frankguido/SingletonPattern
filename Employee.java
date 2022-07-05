dcb5e05e99b5ca1783430b72412cffbc4db203b6
frankguido <38305115+frankguido@users.noreply.github.com>
2022-07-05 17:32:17 -0500

    clase empleado

--- Employee.java
+++ Employee.java
@@ -1,13 +1,5 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package singeton2;
-
-/**
- *
- * @author User
- */
+//Clase empleado
 public class Employee {
     private final String name;
     private final String role;

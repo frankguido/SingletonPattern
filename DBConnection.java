6d443e7f501dad8714ade2fd92b3b9aced9166cb
frankguido <38305115+frankguido@users.noreply.github.com>
2022-07-05 17:35:55 -0500

    clase dbconnection

--- DBConnection.java
+++ DBConnection.java
@@ -6,17 +6,20 @@ import java.sql.DriverManager;
 
 public class DBConnection {
     
-    static Connection conn = null;
+    //Atributo privado y estatico del mismo tipo
+    private static Connection conn = null;
     
+    //constructor privado por defecto que no reciba parametros
     private DBConnection(){
     }
     
+    //metodo publico estatico que retorna el tipo de la clase Connection que no recibe parametros
     public static Connection getDBConnection(){
         try{
             if(conn == null){
                 
                 Class.forName("com.mysql.jdbc.Driver");
-                conn = DriverManager.getConnection("jdbc:mysql://localhost/db_proyectointegradorgroup5", "root", "12345678");  
+                conn = DriverManager.getConnection("jdbc:mysql://localhost/db_proyecto", "root", "12345678");  
             }
             
         } catch (Exception e){
